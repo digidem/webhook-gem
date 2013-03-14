09c01d75ae294e918962880ec7f3b46b9d90a340
Gregor MacLennan <gmaclennan@digital-democracy.org>
2013-03-14 12:46:32 -0700

    Remove unnecessary api_key logic

--- lib/webhook.rb
+++ lib/webhook.rb
@@ -4,12 +4,10 @@ require 'json'
 module Webhook
   # Reads post content and looks up key details from a Stripe event.
   class Stripe
-    @@api_key = nil
     # Makes these class variables accessible via Stripe['type'] etc.
     attr_accessor :type, :email, :name, :amount, :description
     
     def initialize(post_content)
-      ::Stripe.api_key = @@api_key
       # Parse the body content as JSON
       params = JSON.parse(post_content)
       # For more security, retrieve the actual event from Stripe to make sure it really exists.
@@ -33,13 +31,6 @@ module Webhook
       # This adds a method Webhook::Stripe.firstname which returns just the first name.
       self.name.split(' ').first
     end
-    
-    def self.api_key=(api_key)
-      @@api_key = api_key
-    end
-    
-    def self.api_key
-      @@api_key
-    end
+
   end
 end
