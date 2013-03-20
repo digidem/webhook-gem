d380df81307525f5b1158b07492b524b345b5372
Gregor MacLennan <gmaclennan@digital-democracy.org>
2013-03-20 00:49:40 -0700

    Added support for Paypal IPN

--- lib/webhook.rb
+++ lib/webhook.rb
@@ -1,5 +1,6 @@
 require 'stripe'
 require 'json'
+require 'rack/utils'
 
 module Webhook
   # Reads post content and looks up key details from a Stripe event.
@@ -33,5 +34,48 @@ module Webhook
       self.name.split(' ').first
     end
 
+  end
+  
+  class Paypal
+    attr_accessor :type, :email, :name, :amount, :description, :date
+
+    def initialize(post_content)
+      raise NoDataError if post_content.to_s.empty?
+
+      @params  = {}
+      @raw     = ""
+
+      parse(post_content)
+      
+      @email = @params['payer_email']
+      @name = [@params['first_name'], @params['last_name']].join(" ")
+      @amount = @params['mc_gross'].to_f*100
+      @description = @params['item_name']
+      @date = DateTime.parse(@params['payment_date']).to_time
+    end
+    
+    def completed?
+      type == :Completed
+    end
+    
+    def validated?
+      request = RestClient.post "https://www.paypal.com/cgi-bin/webscr", @raw + "&cmd=_notify-validate"
+      raise StandardError.new("Faulty paypal result: #{request.body}") unless ["VERIFIED", "INVALID"].include?(request.body)
+      request.body == "VERIFIED"
+    end
+    
+    private
+    
+    def type
+      @type ||= (@params['payment_status'] ? @params['payment_status'].to_sym : nil)
+    end
+
+    # Take the posted data and move the relevant data into a hash
+    def parse(post_content)
+      @raw = post_content
+      @params = Rack::Utils.parse_query(post_content)
+      # Rack allows duplicate keys in queries, we need to use only the last value here
+      @params.each{|k,v| self.params[k] = v.last if v.is_a?(Array)}
+    end
   end
 end
--- webhook.gemspec
+++ webhook.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
   s.name        = 'webhook'
-  s.version     = '0.1.0'
-  s.date        = '2013-03-14'
+  s.version     = '0.2.0'
+  s.date        = '2013-03-20'
   s.summary     = "A wrapper for Stripe webhooks"
   s.description = "This simple gem will take a webhook post, check the event on Stripe, and get basic details about a charge"
   s.authors     = ["Gregor MacLennan"]
@@ -11,4 +11,5 @@ Gem::Specification.new do |s|
   
   s.add_dependency('stripe', '~>1.7')
   s.add_dependency('json')
+  s.add_dependency('rack')
 end
